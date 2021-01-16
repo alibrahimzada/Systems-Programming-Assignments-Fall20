34279ded1e058cd771539c33432a1b75b0cc3a13
alibrahimzada <alibrahimzada@gmail.com>
2021-01-16 19:49:01 +0300

    parsed trace file

--- Assignment 3/alireza_ibrahimzada__alper_dokay.py
+++ Assignment 3/alireza_ibrahimzada__alper_dokay.py
@@ -1,8 +1,17 @@
 import sys
 import math
 
+hex_binary = {'0': '0000', '1': '0001', '2': '0010', '3': '0011', '4': '0100', '5': '0101', '6': '0110', '7': '0111', 
+              '8': '1000', '9': '1001', 'A': '1010', 'B': '1011', 'C': '1100', 'D': '1101', 'E': '1110', 'F': '1111'}
+
 configs = {'L1 #sets': 0, 'L1 lines/set': 0, 'L1 block size': 0,
 		   'L2 #sets': 0, 'L2 lines/set': 0, 'L2 block size': 0}
+
+eviction_queue = {}
+
+performance = {'L1 instruction hits': 0, 'L1 instruction misses': 0, 'L1 instruction evictions': 0,
+			   'L1 data hits': 0, 'L1 data misses': 0, 'L1 data evictions': 0,
+			   'L2 hits': 0, 'L2 misses': 0, 'L2 evictions': 0}
 
 def parse_arguments(args):
 	trace_file = ''
@@ -31,6 +40,7 @@ def create_cache(total_sets, total_lines, block_size, cache):
 		{line number:
 			{
 				v_bit: 0/1,
+				time : 0,
 				tag: ...,
 				block: [byte_1, ...]
 			}, ...
@@ -39,8 +49,9 @@ def create_cache(total_sets, total_lines, block_size, cache):
 	"""
 	for i in range(total_sets):
 		cache.setdefault(str(i), {})
+		eviction_queue.setdefault(str(i), [])
 		for j in range(total_lines):
-			cache[str(i)].setdefault(str(j), {'v_bit': 0, 'tag': '', 'block': []})
+			cache[str(i)].setdefault(str(j), {'v_bit': 0, 'tag': '', 'time': 0, 'block': []})
 			for k in range(block_size):
 				cache[str(i)][str(j)]['block'].append('')
 
@@ -62,6 +73,18 @@ def parse_memory(aligned_ram):
 
 	return aligned_ram
 
+def hex_to_bin(hex_value):
+	binary_number = ''
+	for hex_number in hex_value:
+		binary_number += hex_binary[hex_number]
+	return binary_number
+
+def bin_to_dec(binary_number):
+	sum_ = 0
+	for i in range(len(binary_number)):
+		sum_ += int(binary_number[i]) * math.pow(2, len(binary_number) - i - 1)
+	return sum_
+
 def main():
 	L1_data = {}
 	L1_instruction = {}
@@ -80,5 +103,23 @@ def main():
 	L2_cache = create_cache(configs['L2 #sets'], configs['L2 lines/set'], configs['L2 block size'], L2_cache)
 
 	aligned_ram = parse_memory(aligned_ram)
+
+
+	with open(trace_file) as tf:
+		for line in tf:
+			trace = line.replace(',', '').strip().split()
+			address = trace[1]
+			size = trace[2]
+			if trace[0] == 'I':   # instruction load
+				pass
+
+			elif trace[0] == 'L':   # data load
+				pass
+
+			elif trace[0] == 'S':   # data store
+				data = trace[3]
+
+			elif trace[0] == 'M':   # data load and then data store
+				data = trace[3]
 
 main()
